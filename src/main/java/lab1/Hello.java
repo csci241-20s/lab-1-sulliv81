e93fb24df1a773d5eac2a603286757408cc428f1
Bo Sullivan <sulliv81@wwu.edu>
2020-04-12 19:11:43 -0700

    edited hello.java

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -1,21 +1,8 @@
 package lab1;
 
 public class Hello {
-<<<<<<< HEAD
-<<<<<<< HEAD
   public static void main(String[] args) {
     System.out.println("Hello, " + args[0]);
-=======
-=======
->>>>>>> 8feb17dd7bd578212adb2472f8e44c789728e2c3
-  public static void main(String[] args) { 
-    for (String s: stringPassed) {
-    
-    System.out.println("Hello " + args[0]);
-    } 
-<<<<<<< HEAD
->>>>>>> 8feb17dd7bd578212adb2472f8e44c789728e2c3
-=======
->>>>>>> 8feb17dd7bd578212adb2472f8e44c789728e2c3
+
   }
 }
